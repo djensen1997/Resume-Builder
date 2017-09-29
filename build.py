4c20489589c50638a62781d67706d0848495e6eb
DaneJensen <firstlegoboy@comcast.net>
2017-09-29 14:53:31 -0400

    parses user input for new contact info and for editing contact info

--- build.py
+++ build.py
@@ -21,6 +21,14 @@ import os
 save = None
 savef = None
 items = []
+STATE_CATAGORY = 0
+STATE_ITEM = 1
+STATE_DESCRIPTION = 2
+STATE_HEADER = 3
+STATE_SKILLS = 4
+STATE_TERM = 5
+MODE_EDIT = 0
+MODE_NEW = 1
 
 def parsedat():
     """
@@ -35,6 +43,8 @@ def parsedat():
             continue
         else:
             line = line.strip()
+            if len(line) == 0:
+                continue
             if item == 0:#not parsing an item
                 try:
                     #print("making a dict for {}".format(line))
@@ -69,20 +79,18 @@ def inputdat():
     item = []
     prompt = ""
     output = ""
-    STATE_CATAGORY = 0
-    STATE_ITEM = 1
-    STATE_DESCRIPTION = 2
-    STATE_HEADER = 3
-    STATE_SKILLS = 4
-    STATE_TERM = 5
+    mode = MODE_NEW
     if len(items) > 0:
         for i in range(0, len(items)):
+            item = items[i]
             if item[0] == "contact":
                 item = items[i]
-                prompt = "Edit Contact Info?"
+                prompt = "Edit Contact Info? (y or n)  "
+                mode = MODE_EDIT
         if item == []:
             prompt = "np"
             output = "Collecting Contact Info"
+            mode = MODE_NEW
     state = STATE_HEADER
     while state != STATE_TERM:
         value = None
@@ -91,8 +99,71 @@ def inputdat():
         else:
             value = input(prompt)
         if state == STATE_HEADER:
+            order = [
+                "name",
+                "email",
+                "phone",
+                "addr1",
+                "addr2",
+                "other",
+            ]
+            aorder = [
+                'street addr',
+                'line2',
+                'city',
+                'state',
+                'zip',
+            ]
+            if mode == MODE_EDIT and value == 'y':
+                #order
+                #print (item)
+                for i in order:
+                    print("{} = {}".format(i, item[1][i]))
+                    value = input("Would you like to Change This? (y or n)  ")
+                    if value == 'y':
+                        if i.find('addr', 0, len(i)) >= 0:
+                            temp = {}
+                            for j in aorder:
+                                temp[j] = input('{}?  '.format(j))
+                            out = ''
+                            if temp['line2'] != '':
+                                out = '{},<br>{},<br>'.format(temp['street addr'], temp['line2'])
+                            else:
+                                out = '{},<br>'.format(temp['street addr'])
+                            #print (temp)
+                            out += '{}, {}, {}'.format(temp['city'], temp['state'], temp['zip'])
+                            item[1][i] = out
+                        else:
+                            item[1][i] = input("Type new value:  ")
+                    else:
+                        #nothing special for no
+                        pass
+            elif mode == MODE_NEW:
+                for i in order:
+                    if i.find('addr', 0, len(i)) >= 0:
+                        temp = {}
+                        for j in aorder:
+                            temp[j] = input('{}?  '.format(j))
+                        out = ''
+                        if temp['line2'] != '':
+                            out = '{},<br>{},<br>'.format(temp['street addr'], temp['line2'])
+                        else:
+                            out = '{},<br>'.format(temp['street addr'])
+                        #print (temp)
+                        out += '{}, {}, {}'.format(temp['city'], temp['state'], temp['zip'])
+                        item[1][i] = out
+                    else:
+                        item[1][i] = input("{}:  ".format(i))
+
+        elif state == STATE_CATAGORY:
             pass
 
+        value = input("Continue? (y or n)  ")
+        if value == 'n':
+            state = STATE_TERM
+        else:
+            state = STATE_CATAGORY
+            mode = MODE_NEW
 
 
 def savedat():
@@ -105,6 +176,8 @@ def savedat():
         savef.write('{} = {{\n'.format(item[0]))
         l = item[1].items()
         for pair in l:
+            if(pair[1] == ""):
+                continue
             savef.write('\t{} = {}\n'.format(pair[0], pair[1]))
         savef.write('}\n')
 
